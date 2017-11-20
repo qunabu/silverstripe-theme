1373a04f05faa86226c6579a482ae6c4c87a640e
Mateusz Qunabu <mateusz@qunabu.com>
2017-11-20 08:57:42 +0100

    Update entry.js

--- javascript/es6/entry.js
+++ javascript/es6/entry.js
@@ -1,5 +1,10 @@
 /** import sections */
 import Grid from './dev/grid';
+import PageTransitions from './dev/page_transitions';
+import Body from './dev/body';
 
 /** attaching behaviors to global object */
 window.SilverStripe.behaviors.Grid = new Grid();
+window.SilverStripe.behaviors.PageTransitions = new PageTransitions();
+window.SilverStripe.behaviors.Body = new Body();
+
